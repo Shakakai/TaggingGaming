b0dcc8e651e69085ef95fa344cd0aa92668d29b0
Volt <Volt@absoluteterritory.org>
2011-07-18 20:59:17 -0400

    More speed; more power

--- src/Tag.hs
+++ src/Tag.hs
@@ -19,22 +19,12 @@ import qualified Data.ByteString as BS
 import Debug.Trace
 
 traceShow' x = traceShow x x
-
 traceSortShow' x = traceShow' $ sortBy (comparing snd) x
 
-cleanAndRemoveComments ls = filter (not . Text.null) $ map removeComments ls
-    where
-        removeComments str = Text.stripEnd $ fst $ Text.breakOn "|" str
-
-readStoplist path = 
-    fmap (cleanAndRemoveComments . Text.lines . TSE.decodeUtf8) $
-        BS.readFile path
-
-corpusFilesPreprocessed stopSet dir = do
+corpusFilesPreprocessed dir = do
         paths <- readDirectory dir
-        fmap (map (first Text.pack >>> second preprocessDocument)) $ mapM nameAndContents paths
-    where
-        removeStopWords = filter (\x → not $ x `Set.member` stopSet)
+        fmap (map (first Text.pack >>> second preprocessDocument)) $
+            mapM nameAndContents paths
 
 preprocessDocument = tokenise . clean . toLower
 
@@ -45,7 +35,7 @@ mkTfidfs idf contents = (\word docname → max 0 $ calculateTfidf word docname)
             Nothing → 0
             Just tfMap → Map.findWithDefault 0 word tfMap
         calculateTfidf word docname = (tf word docname) * (idf word)
-        
+
 mkIdf contents = (\word → calculateIdf word)
     where
         calculateIdf word   | df' == 0 = 2**32
@@ -59,18 +49,22 @@ tfidfVector vocab tfidfs (name, d) = V.fromList $
     map snd $ zip [1..] $ map (\w -> (tfidfs w name)) $
     Set.toList vocab
 
-bestMatch vocab idf tfidfs submission corpus =
+bestMatch idf tfidfs submission corpus =
         chooseBest $ map similarity corpus
     where
-        similarity gameDoc@(name, _) = (name, sim)
+        similarity gameDoc@(name, gameDocText) = (name, sim)
             where
+                vocab = redditVocab `Set.union` gameVocab
+                    where gameVocab = Set.fromList gameDocText
                 sim = vectorCosineSimilarity submissionVector gameVector
-                submissionVector =
-                    denseFeaturesFromDocument vocab $
-                    Map.map (max 0.0) $
-                    Map.mapWithKey (\word tf → tf * (idf word)) $
-                    histogram $ snd submission
                 gameVector = tfidfVector vocab tfidfs gameDoc
+                submissionVector = denseFeaturesFromDocument vocab
+                    submissionSparseVector
+        submissionSparseVector =
+            Map.map (max 0.0) $
+            Map.mapWithKey (\word tf → tf * (idf word)) $
+            histogram $ snd submission
+        redditVocab = Map.keysSet submissionSparseVector
         chooseBest xs = choose' m (zscore $ snd m) (zscore $ snd m2)
             where
                 m = maximumBy (comparing snd) xs
@@ -79,21 +73,17 @@ bestMatch vocab idf tfidfs submission corpus =
                 stddev = sqrt $ sum $ map (\x → (mean - x) **2) xs'
                 mean = (sum xs') / (fromIntegral $ length xs')
                 xs' = map snd xs
-                choose' (x, sim) zscore1 zscore2 = if zscore1/zscore2 > 1.05
+                choose' (x, sim) zscore1 zscore2 = if zscore1/zscore2 > 1.5
                     then Just ("http://en.wikipedia.org/wiki/" <> x)
                     else Nothing
 
 main = do
-    wikipediaVocabulary <- vocabulary "../game_corpus"
-    redditVocabulary <- vocabulary "../reddit_examples"
-    stopSet <- fmap (Set.fromList) $ readStoplist "../english_stop.txt"
-    let vocab = Set.union wikipediaVocabulary redditVocabulary
-    gameCorpusPreprocessed <- corpusFilesPreprocessed stopSet "../game_corpus"
-    redditCorpusPreprocessed <- corpusFilesPreprocessed stopSet "../reddit_examples"
-    let idf = mkIdf  (gameCorpusPreprocessed ++ redditCorpusPreprocessed)
+    gameCorpusPreprocessed <- corpusFilesPreprocessed "../game_corpus"
+    redditCorpusPreprocessed <- corpusFilesPreprocessed "../reddit_examples"
+    let idf = mkIdf (gameCorpusPreprocessed ++ redditCorpusPreprocessed)
     let tfidfs =
             mkTfidfs idf (gameCorpusPreprocessed ++ redditCorpusPreprocessed)
     forM_ redditCorpusPreprocessed $ \submission → do
-        case bestMatch vocab idf tfidfs submission gameCorpusPreprocessed of
+        case bestMatch idf tfidfs submission gameCorpusPreprocessed of
             Nothing → putStrLn "No luck"
             Just x → putStrLn $ dropExtension . Text.unpack $ x
